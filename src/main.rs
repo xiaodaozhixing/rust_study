9792b7752008e52f92665ea6456a37041bcf708e
Auspicious Chan <auspiciouschan1@gmail.com>
2021-12-05 01:46:10 +0800

    feat: print_test

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
 fn main() {
-    println!("Hello, world!");
+    print_test()
+}
+
+fn print_test() {
+    let value0 = "value0";
+    let value1 = "value1";
+    print!("print {}", value0);
+    println!("println value0: {}", value0);
+    println!("println value0: {} value0: {0}", value0);
+    println!("println value0: {0} value0: {}", value0);
+    println!("println value0: {0} value1: {1}", value0, value1);
+    println!("println value0: {} value1: {1}", value0, value1);
+    println!("println value1: {1} value0: {}", value0, value1);
 }
